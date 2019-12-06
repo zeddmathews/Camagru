289a6dad7556ce0236ec5a9599dbcf5beb6286e6
Zain MATHEWS <zmathews@c4r18s4.wethinkcode.co.za>
2019-12-06 16:30:21 +0200

    Sooooo much work....totally

--- likes.php
+++ likes.php
@@ -1,5 +1,12 @@
 <?php include('header.php')?>
 <?php
-	
+	require('config/pdo_connection');
+
+	try {
+		
+	}
+	catch(PDOException $e) {
+		echo $e->getMessage();
+	}
 ?>
 <?php include('footer.php')?>
