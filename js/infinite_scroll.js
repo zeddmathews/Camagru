b535b3816aa30f1fd3282553ff52383e2ef5115d
Zain MATHEWS <zmathews@c4r19s4.wethinkcode.co.za>
2019-12-13 18:24:04 +0200

    Dafuq

--- js/infinite_scroll.js
+++ js/infinite_scroll.js
@@ -1,5 +1,4 @@
 var load_id = 1;
-
 window.addEventListener("load", () => {
 	loadMore()
 });
@@ -7,11 +6,14 @@ window.addEventListener("load", () => {
 loadMore = () => {
 	var index = document.getElementById('gallery');
 	var xhr = new XMLHttpRequest();
+	console.log('oi');
 	if (index) {
+		xhr.onload = () => {
 		if (xhr.status == 200) {
 			load_id++;
 		}
-		xhr.open('POST', 'scroll.php?load_id='+load_id);
+		xhr.open('POST', '/Camagru/scroll.php?load_id='+load_id);
 		xhr.send();
+		}
 	}
 };
---
+++ js/likes.js
@@ -0,0 +1,25 @@
+function like(id)
+{
+	var request = new XMLHttpRequest();
+
+	request.onload = function(response)
+	{
+
+		if (request.status === 200)
+		{
+			var likes = document.getElementById("num_likes-"+id);
+			likes.innerHTML = 1 + Number(likes.innerHTML);
+		}
+		else if (request.status === 205)
+		{
+			var likes = document.getElementById("num_likes-"+id);
+			likes.innerHTML = Number(likes.innerHTML) - 1;
+		}
+		else
+			console.log(request.responseText);
+
+	}
+	request.open("POST", "like.php");
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.send('pid='+id);
+}
